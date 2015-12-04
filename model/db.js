40082ae616dabf1557f577ac4bf5d725a41cba45
Nguyen Quang Minh <quangminh.nguyen@mail.utoronto.ca>
2015-12-04 16:56:36 -0500

    fixed fb call back link

--- model/db.js
+++ model/db.js
@@ -1,2 +1,11 @@
+var http = require ('http');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/a5db');
+var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/a5db';
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
